9d2a040baf2e9a7574055b7cdb821d396a1af1bf
Egor <youcankeepsilence@gmail.com>
2018-08-12 16:33:39 +0300

    pull commit

--- backend/src/flask-test.py
+++ backend/src/flask-test.py
@@ -4,6 +4,7 @@ from src.logger import Logger
 from src.settings import Settings
 import os
 from flask_bootstrap import Bootstrap
+from flask import jsonify
 
 env = Environment(
     loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates/")),
@@ -35,5 +36,10 @@ def add():
 
 @app.route('/config')
 def get_config():
+    return jsonify(Settings().load_current_server_config())
+
+
+@app.route('/ui/config')
+def get_ui_config():
     return env.get_template('config.html').render(config=Settings().load_current_server_config())
 
---
+++ frontend/settings-ui/src/components/Settings.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        {{data}}
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue, Inject} from 'vue-property-decorator';
+    import axios from 'axios';
+
+    @Component
+    export default class Settings extends Vue {
+        // @Inject('getCurrentSettings') private getCurrentSettings: any;
+        @Prop() private data: any;
+
+        private created() {
+            axios.get('http://127.0.0.1:5000/config', {
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                }}).then(answer => {
+                this.data = answer.data;
+                console.log(answer);
+            });
+            // this.getCurrentSettings.then((d: any) => {
+            //     console.log(d.data);
+            //     this.data = d.data;
+            // });
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
