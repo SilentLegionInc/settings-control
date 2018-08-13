3be604d90101d64e8e4912bd696940d6eeb46c43
Befezdow <befezd@gmail.com>
2018-08-14 02:06:42 +0300

    played with animations

--- frontend/settings-ui/src/components/Test.vue
+++ frontend/settings-ui/src/components/Test.vue
@@ -4,33 +4,12 @@
             Test page
         </h1>
 
-        <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies ex sit amet magna tincidunt, a consectetur ipsum sollicitudin. Phasellus dapibus tempus condimentum. Nullam elementum venenatis sapien, a imperdiet nulla porta sit amet. Nam velit turpis, interdum eu velit quis, tempor sollicitudin orci. Ut non quam ut neque suscipit condimentum a ac urna. Sed dapibus urna vel nisi cursus interdum. Etiam in felis aliquet, mattis eros nec, ullamcorper tellus. Sed sed justo tincidunt, pretium eros quis, sollicitudin leo. Morbi sit amet magna non felis blandit vestibulum eget in velit. Nunc nec lobortis eros.
-            Ut auctor imperdiet lorem nec facilisis. Vestibulum porttitor egestas sapien eget eleifend. Proin ut ex ac nisl porta interdum. Aliquam vel pellentesque nibh. Proin id elementum orci. Vestibulum a condimentum turpis. Etiam mattis bibendum quam. Pellentesque pulvinar nulla vitae justo egestas placerat. Aenean mollis massa nec interdum placerat. Maecenas et posuere massa, nec iaculis ex. Pellentesque in nunc vehicula mi ullamcorper consequat. Maecenas eleifend lorem ac blandit vestibulum. Sed mattis justo at sollicitudin mollis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-            Sed et dui cursus, tempor felis nec, elementum libero. Sed vel dui porta tellus euismod mattis. Vivamus ullamcorper metus in molestie accumsan. Integer dapibus et eros ac cursus. Donec in lobortis sapien. Cras tempor, urna congue consequat elementum, odio enim pellentesque enim, vel ornare est erat ut risus. Nulla facilisi. Mauris interdum ut diam sit amet feugiat. Etiam malesuada pretium sodales. Pellentesque posuere quam egestas nunc aliquam, id egestas enim pharetra. Fusce sit amet tincidunt diam. Aliquam condimentum metus ut tortor bibendum dignissim. Suspendisse potenti. Aenean in efficitur ex. Nam id mauris aliquam, pellentesque leo in, convallis augue.
-            Nullam quis ex dolor. Maecenas facilisis lorem fermentum nibh bibendum, in porttitor lectus feugiat. Fusce eleifend posuere aliquet. Sed ante mauris, convallis nec accumsan at, consectetur vel nulla. Phasellus at iaculis turpis, eget faucibus metus. Morbi at leo sit amet dolor viverra lobortis. Curabitur sed cursus nisi, non posuere quam. Fusce gravida facilisis odio, vel consequat magna condimentum id.
-            Curabitur imperdiet, nisl a pellentesque tincidunt, nibh massa condimentum arcu, vitae faucibus neque turpis vitae nisl. Cras sit amet nisi velit. Proin ac ultricies dui. Integer aliquam, massa sed euismod gravida, ipsum diam luctus mauris, ut imperdiet quam nulla vitae lectus. Vestibulum gravida tempus erat, nec pretium mauris tristique ac. Praesent eget justo nisl. Nulla lobortis nunc sit amet leo cursus venenatis. Maecenas vitae tellus egestas, imperdiet arcu vel, venenatis diam.
-        </div>
-
         <hr>
-
-        <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies ex sit amet magna tincidunt, a consectetur ipsum sollicitudin. Phasellus dapibus tempus condimentum. Nullam elementum venenatis sapien, a imperdiet nulla porta sit amet. Nam velit turpis, interdum eu velit quis, tempor sollicitudin orci. Ut non quam ut neque suscipit condimentum a ac urna. Sed dapibus urna vel nisi cursus interdum. Etiam in felis aliquet, mattis eros nec, ullamcorper tellus. Sed sed justo tincidunt, pretium eros quis, sollicitudin leo. Morbi sit amet magna non felis blandit vestibulum eget in velit. Nunc nec lobortis eros.
-            Ut auctor imperdiet lorem nec facilisis. Vestibulum porttitor egestas sapien eget eleifend. Proin ut ex ac nisl porta interdum. Aliquam vel pellentesque nibh. Proin id elementum orci. Vestibulum a condimentum turpis. Etiam mattis bibendum quam. Pellentesque pulvinar nulla vitae justo egestas placerat. Aenean mollis massa nec interdum placerat. Maecenas et posuere massa, nec iaculis ex. Pellentesque in nunc vehicula mi ullamcorper consequat. Maecenas eleifend lorem ac blandit vestibulum. Sed mattis justo at sollicitudin mollis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-            Sed et dui cursus, tempor felis nec, elementum libero. Sed vel dui porta tellus euismod mattis. Vivamus ullamcorper metus in molestie accumsan. Integer dapibus et eros ac cursus. Donec in lobortis sapien. Cras tempor, urna congue consequat elementum, odio enim pellentesque enim, vel ornare est erat ut risus. Nulla facilisi. Mauris interdum ut diam sit amet feugiat. Etiam malesuada pretium sodales. Pellentesque posuere quam egestas nunc aliquam, id egestas enim pharetra. Fusce sit amet tincidunt diam. Aliquam condimentum metus ut tortor bibendum dignissim. Suspendisse potenti. Aenean in efficitur ex. Nam id mauris aliquam, pellentesque leo in, convallis augue.
-            Nullam quis ex dolor. Maecenas facilisis lorem fermentum nibh bibendum, in porttitor lectus feugiat. Fusce eleifend posuere aliquet. Sed ante mauris, convallis nec accumsan at, consectetur vel nulla. Phasellus at iaculis turpis, eget faucibus metus. Morbi at leo sit amet dolor viverra lobortis. Curabitur sed cursus nisi, non posuere quam. Fusce gravida facilisis odio, vel consequat magna condimentum id.
-            Curabitur imperdiet, nisl a pellentesque tincidunt, nibh massa condimentum arcu, vitae faucibus neque turpis vitae nisl. Cras sit amet nisi velit. Proin ac ultricies dui. Integer aliquam, massa sed euismod gravida, ipsum diam luctus mauris, ut imperdiet quam nulla vitae lectus. Vestibulum gravida tempus erat, nec pretium mauris tristique ac. Praesent eget justo nisl. Nulla lobortis nunc sit amet leo cursus venenatis. Maecenas vitae tellus egestas, imperdiet arcu vel, venenatis diam.
-        </div>
-
-        <hr>
-
-        <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies ex sit amet magna tincidunt, a consectetur ipsum sollicitudin. Phasellus dapibus tempus condimentum. Nullam elementum venenatis sapien, a imperdiet nulla porta sit amet. Nam velit turpis, interdum eu velit quis, tempor sollicitudin orci. Ut non quam ut neque suscipit condimentum a ac urna. Sed dapibus urna vel nisi cursus interdum. Etiam in felis aliquet, mattis eros nec, ullamcorper tellus. Sed sed justo tincidunt, pretium eros quis, sollicitudin leo. Morbi sit amet magna non felis blandit vestibulum eget in velit. Nunc nec lobortis eros.
-            Ut auctor imperdiet lorem nec facilisis. Vestibulum porttitor egestas sapien eget eleifend. Proin ut ex ac nisl porta interdum. Aliquam vel pellentesque nibh. Proin id elementum orci. Vestibulum a condimentum turpis. Etiam mattis bibendum quam. Pellentesque pulvinar nulla vitae justo egestas placerat. Aenean mollis massa nec interdum placerat. Maecenas et posuere massa, nec iaculis ex. Pellentesque in nunc vehicula mi ullamcorper consequat. Maecenas eleifend lorem ac blandit vestibulum. Sed mattis justo at sollicitudin mollis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-            Sed et dui cursus, tempor felis nec, elementum libero. Sed vel dui porta tellus euismod mattis. Vivamus ullamcorper metus in molestie accumsan. Integer dapibus et eros ac cursus. Donec in lobortis sapien. Cras tempor, urna congue consequat elementum, odio enim pellentesque enim, vel ornare est erat ut risus. Nulla facilisi. Mauris interdum ut diam sit amet feugiat. Etiam malesuada pretium sodales. Pellentesque posuere quam egestas nunc aliquam, id egestas enim pharetra. Fusce sit amet tincidunt diam. Aliquam condimentum metus ut tortor bibendum dignissim. Suspendisse potenti. Aenean in efficitur ex. Nam id mauris aliquam, pellentesque leo in, convallis augue.
-            Nullam quis ex dolor. Maecenas facilisis lorem fermentum nibh bibendum, in porttitor lectus feugiat. Fusce eleifend posuere aliquet. Sed ante mauris, convallis nec accumsan at, consectetur vel nulla. Phasellus at iaculis turpis, eget faucibus metus. Morbi at leo sit amet dolor viverra lobortis. Curabitur sed cursus nisi, non posuere quam. Fusce gravida facilisis odio, vel consequat magna condimentum id.
-            Curabitur imperdiet, nisl a pellentesque tincidunt, nibh massa condimentum arcu, vitae faucibus neque turpis vitae nisl. Cras sit amet nisi velit. Proin ac ultricies dui. Integer aliquam, massa sed euismod gravida, ipsum diam luctus mauris, ut imperdiet quam nulla vitae lectus. Vestibulum gravida tempus erat, nec pretium mauris tristique ac. Praesent eget justo nisl. Nulla lobortis nunc sit amet leo cursus venenatis. Maecenas vitae tellus egestas, imperdiet arcu vel, venenatis diam.
-        </div>
+        <i class="fa fa-tint my-anim" :class="{ 'my-anim-selected': activeElement === 0 }" @click="clicked(0)"></i><br>
+        <i class="fa fa-male my-anim" :class="{ 'my-anim-selected': activeElement === 1 }" @click="clicked(1)"></i><br>
+        <i class="fa fa-lock my-anim" :class="{ 'my-anim-selected': activeElement === 2 }" @click="clicked(2)"></i><br>
+        <i class="fa fa-user my-anim" :class="{ 'my-anim-selected': activeElement === 3 }" @click="clicked(3)"></i><br>
+        <i class="fa fa-plus my-anim" :class="{ 'my-anim-selected': activeElement === 4 }" @click="clicked(4)"></i><br>
 
     </div>
 </template>
@@ -39,8 +18,31 @@
     import { Component, Vue } from 'vue-property-decorator';
 
     @Component
-    export default class Test extends Vue {}
+    export default class Test extends Vue {
+        private activeElement: number = -1;
+
+        private clicked(index: number) {
+            index = Number(index);
+            if (this.activeElement === index) {
+                this.activeElement = -1;
+            } else {
+                this.activeElement = index;
+            }
+        }
+    }
 </script>
 
 <style scoped lang="scss">
+    .my-anim {
+        transition: transform 1s ease, font-size 1s ease, color 1s ease;
+        font-size: 50px;
+        margin-bottom: 10px;
+        color: #10707f;
+    }
+
+    .my-anim-selected {
+        transform: translateX(50px) rotateZ(45deg);
+        font-size: 70px;
+        color: #007bff;
+    }
 </style>
