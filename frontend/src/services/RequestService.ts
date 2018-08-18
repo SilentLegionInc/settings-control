f4533239144c2fa7187861adc730ee7e725b7b63
Befezdow <befezd@gmail.com>
2018-08-18 17:23:29 +0300

    fixed lintner

--- frontend/src/services/RequestService.ts
+++ frontend/src/services/RequestService.ts
@@ -1,5 +1,5 @@
-import {Prop, Provide} from 'vue-property-decorator';
-import axios, {AxiosPromise} from 'axios';
+import { Prop, Provide } from 'vue-property-decorator';
+import axios, { AxiosPromise } from 'axios';
 
 export class RequestService {
     private serverPath: string = 'http://127.0.0.1:5000/config';
