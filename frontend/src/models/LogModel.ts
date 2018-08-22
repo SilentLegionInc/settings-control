80107d4bf20d39522c339b68abe526a21281af9a
Befezdow <befezd@gmail.com>
2018-08-22 22:29:40 +0300

    forget model

--- frontend/src/models/LogModel.ts
+++ frontend/src/models/LogModel.ts
@@ -1,17 +1,17 @@
 export class LogModel {
-    constructor(id: number = 0, type: LogLevel = 0, title: string = '', message: string = '', time: Date = new Date()) {
+    constructor(id: number = 0, time: Date = new Date(), type: LogLevel = 0, title: string = '', message: string = '') {
         this.id = id;
+        this.time = time;
         this.type = type;
         this.title = title;
         this.message = message;
-        this.time = time;
     }
 
     public id: number = 0;
+    public time: Date = new Date();
     public type: LogLevel = 0;
     public title: string = '';
     public message: string = '';
-    public time: Date = new Date();
 }
 
 export enum LogLevel {
