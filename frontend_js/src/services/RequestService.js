60c33bfbfe53bdd44c6cf63ceb04f69754151d24
Silence <youcankeepsilence@gmail.com>
2019-03-30 18:16:02 +0000

    oops

--- frontend_js/src/services/RequestService.js
+++ frontend_js/src/services/RequestService.js
@@ -87,7 +87,8 @@ export class RequestService {
         const path = this._constructPath('api/network');
         const res = await axios.get(path);
         if (res.status === 200) {
-            return MapperService.mapNetworksResponse(res.data);} else {
+            return MapperService.mapNetworksResponse(res.data);
+        } else {
             Logger.error(res.data.errorInfo);
             throw new ServerExceptionModel(res.data.errorInfo, res.status);
         }
