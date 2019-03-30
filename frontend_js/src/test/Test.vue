4d83995e731584f6083d7f6342d9ff45d87c701f
Befezdow <befezd@gmail.com>
2019-03-31 01:13:06 +0300

    kek

--- frontend_js/src/test/Test.vue
+++ frontend_js/src/test/Test.vue
@@ -5,22 +5,21 @@
         <test-chart></test-chart>
         <hr>
         <test-select></test-select>
-        <div class="my_container">
-            <span style="flex-grow: 2"><hr></span><span style="flex-basis: auto" class="ml-2 mr-2">ОПРЛДЫОВАТЫОЛВИОЛЫВИТЛОЫВАТЫВЛОИТЛОЫВТАЛОЫВТМЛОЫВТМЛОЫВТМЛОДЫВМТЫЛДОВМТЫЛВО</span><span style="flex-grow: 2"><hr></span>
-        </div>
     </div>
 </template>
 <script>
 import TestChart from './TestChart.vue';
 import TestSelect from './TestSelect';
 import TestAnimation from './TestAnimation';
+import Divider from '../components/Divider';
 
 export default {
     name: 'Test',
     components: {
         'test-chart': TestChart,
         'test-select': TestSelect,
-        'test-animation': TestAnimation
+        'test-animation': TestAnimation,
+        'divider': Divider
     }
 }
 </script>
